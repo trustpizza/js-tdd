c47f3b5a76d8cf1f452b5e1f35a1c21d8e90d1bc
trustpizza <maxelolsson@gmail.com>
2023-03-16 12:02:11 -0500

    Its messsy but it works?

--- tdd-practice/caesar-cypher.js
+++ tdd-practice/caesar-cypher.js
@@ -1,5 +1,56 @@
-function caesarCipher(string, shift) {
-    return string;
+function caesarCipher(string, shift = 0) {
+    if (shift > 26) { shift = shift-26 }
+
+    const returnStringArray = []
+    // First translate letters to CharCodes
+    for (let i = 0; i < string.length; i++) {
+        const el = string[i];
+
+        if (
+            (string.charCodeAt(i) >=65 && string.charCodeAt(i) <=90) ||
+            (string.charCodeAt(i) >=97 && string.charCodeAt(i) <= 122)
+        ) {
+            const originalCode = el.charCodeAt(0);
+            const incrementedCharCode = originalCode + shift;
+            const cleanedCharCode = preventZOverflow(incrementedCharCode, originalCode);
+            const newLetter = translateCharCode(cleanedCharCode);
+            returnStringArray[i]=newLetter;
+        } else {
+            returnStringArray[i]=el
+        }
+    }
+
+    return returnStringArray.join("");
 };
+
+function preventZOverflow(code, originalCode) {
+    if (originalCode >= 65 && originalCode <= 90) {
+        return cleanUppercase(code);
+    } else if (originalCode >= 97 && originalCode <= 122) {
+        return cleanLowercase(code);
+    } else {
+        return code;
+    }
+}
+
+function cleanLowercase(code) {
+    if (code > 122) {
+        return code-26;
+    } else {
+        return code;
+    }
+};
+
+function cleanUppercase(code) {
+    if (code > 90) {
+        return code-26;
+    } else {
+        return code
+    }
+}
+
+function translateCharCode(code) {
+    return String.fromCharCode(code);
+}
 
 module.exports = caesarCipher;
--- tdd-practice/caesar-cypher.test.js
+++ tdd-practice/caesar-cypher.test.js
@@ -4,18 +4,34 @@ test("Shift by 1", () => {
     expect(caesarCipher("abcd", 1)).toBe("bcde")
 });
 
-test.skip("handle Z overflow", () => {
+test("handle Z overflow", () => {
     expect(caesarCipher("xyz", 1)).toBe("yza");
 })
 
-test.skip("Handle Capitalized Letters", () => {
+test("Handle Capitalized Letters", () => {
     expect(caesarCipher("aBcD",1)).toBe("bCdE")
 });
 
-test.skip("Handle a shift of 26", () => {
+test("Handle a shift of 26", () => {
     expect(caesarCipher("abc", 26)).toBe("abc")
 })
 
-test.skip("Handle a shift of over 26", () => {
+test("Handle a shift of over 26", () => {
     expect(caesarCipher("abc", 27)).toBe("bcd")
 });
+
+test("Default shift is 0", () => {
+    expect(caesarCipher("abc")).toBe("abc")
+});
+
+test("Handle Z overflow of capital letters", () => {
+    expect(caesarCipher("XYZ",1)).toBe("YZA");
+});
+
+test("! stays !", () => {
+    expect(caesarCipher("!",11)).toBe("!")
+})
+
+test("Handle punctuation", () => {
+    expect(caesarCipher("abcABC!@#$%^&*(){}[]",27)).toBe("bcdBCD!@#$%^&*(){}[]")
+})
