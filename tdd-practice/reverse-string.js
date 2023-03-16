c19c22298bbee52afc8ed851111021b7cc459428
trustpizza <maxelolsson@gmail.com>
2023-03-16 10:47:44 -0500

    Finish reverse-string

--- tdd-practice/reverse-string.js
+++ tdd-practice/reverse-string.js
@@ -1,5 +1,12 @@
 function reverseString(string) {
-    return string;
+    // Can put into an array and return the charAt() of the array
+    const returnStringArray = []
+    for (let i = 0; i < string.length; i++) {
+        const element = string[i];
+        returnStringArray.unshift(element)
+    }
+
+    return returnStringArray.join("");
 }
 
 module.exports = reverseString;
--- tdd-practice/reverse-string.test.js
+++ tdd-practice/reverse-string.test.js
@@ -1,5 +1,9 @@
 const reverseString = require("./reverse-string")
 
 test("racecar stays racecar", () => {
-    expect(reverseString("racecar").toBe("racecar"))
-})
+    expect(reverseString("racecar")).toBe("racecar")
+});
+
+test("asdf becomes fdsa", () => {
+    expect(reverseString('asdf')).toBe("fdsa")
+});
